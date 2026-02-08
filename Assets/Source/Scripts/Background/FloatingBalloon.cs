6de04ed562dbb590d8e92b178da8b3661351c38a
Yauheni <  eadamovich@justmoby.com>
2026-02-08 23:57:01 +0300

    a little refactoring

--- Assets/Source/Scripts/Background/FloatingBalloon.cs
+++ Assets/Source/Scripts/Background/FloatingBalloon.cs
@@ -5,20 +5,19 @@ using Random = UnityEngine.Random;
 
 public class FloatingBalloon : MonoPooled
 {
-    private const int PathSegments = 60;
-    private const float DefaultAmplitude = 1f;
-    private const float DefaultFrequency = 1.5f;
-    
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Sprite[] _balloonSprites;
 
-    private Action<FloatingBalloon> _onExited;
-
+    private const int PathSegments = 60;
+    private const float DefaultAmplitude = 1f;
+    private const float DefaultFrequency = 1.5f;
     private const float AppearDuration = 0.35f;
     private const float DisappearDuration = 0.25f;
 
     private Tween _scaleTween;
     private Tween _moveTween;
+    
+    private Action<FloatingBalloon> _onExited;
 
     public void Setup(float startX, float endX, float startY, float speed,
         Action<FloatingBalloon> onExited)
